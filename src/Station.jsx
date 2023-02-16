388e2716c77c96987e1e09517581210e3a6bd13d
Aran Asaiesh <aran.asaiesh@chasacademy.se>
2023-02-16 11:31:56 +0100

    Fixed the colors from SR

--- src/Station.jsx
+++ src/Station.jsx
@@ -2,7 +2,7 @@ export default function Station(props) {
   const { channel } = props;
 
   return (
-    <div className="bg-red-500">
+    <div style={{ backgroundColor: `#${channel.color}` }}>
       <img src={channel.image} width={100} height={100} />
       {channel.name}
       <audio controls src={channel.liveaudio.url} type="audio/mpeg" />
